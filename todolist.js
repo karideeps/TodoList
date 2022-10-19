b5d1f18ecb09eedab29d6574f1df58cb0e684e61
Deepak Karivelil <deepakkarivelil@gmail.com>
2022-10-19 14:27:55 -0300

    completed TodoList

--- todolist.js
+++ todolist.js
@@ -93,6 +93,55 @@ class TodoList {
     return resultArray.join(`\n`);
   }
 
+  forEach(callback) {
+    for (let i = 0; i < this.size(); i += 1) {
+      callback(this.todos[i]);
+    }
+  }
+
+  filter(callback) {
+    let completedList = new TodoList(this.title);
+
+    this.forEach(function (todo) {
+      if (callback(todo)) {
+        completedList.add(todo);
+      }
+    });
+
+    return completedList;
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    let todo = this.findByTitle(title);
+    if (todo) {
+      todo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
+
   _validateIndex(index) {
     if(!(index in this.todos)) {
       throw new ReferenceError(`invalid index: ${index}`);
